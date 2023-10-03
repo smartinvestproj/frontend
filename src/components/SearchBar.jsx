ccc101a8eac1ad55e33703470678720405d54db7
Pedro <pedrodinisconceicao@gmail.com>
2023-10-03 22:15:37 +0100

    SearchBar values from API

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/components-styles/searchBar.css'
 
 function SearchBar() {
-  const initialResults = [
-    { name: "Netflix", symbol: "NFLX", value: "€429,70", percentage: "-8,92%" },
-    { name: "Amazon", symbol: "AMZN", value: "€781,82", percentage: "-3.37%" },
-    { name: "Tesla", symbol: "TSLA", value: "€612,30", percentage: "-5.52%" },
-    { name: "Meta", symbol: "META", value: "€139,69", percentage: "-7.49%" },
-    { name: "Apple", symbol: "AAPL", value: "€981,10", percentage: "-6.21%" },
-    { name: "Toyota", symbol: "TYTA", value: "€354,96", percentage: "-2.08%" },
-  ];
+  const URL = "http://127.0.0.1:8000/api/stocks";
+
   const [search, setSearch] = useState("");
+  const [originalResults, setOriginalResults] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        if (response.ok) {
+          const data = await response.json();
+          setOriginalResults(data.data);
+        } else {
+          console.error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   const handleSearch = (e) => {
     const search = e.target.value;
     setSearch(search);
 
-    const filteredResults = search
-      ? initialResults.filter((result) =>
-          result.name.toLowerCase().startsWith(search.toLowerCase())
-        )
-      : [];
+    if (search === "") {
+      setSearchResults([]);
+    } else {
+      const filteredResults = originalResults.filter((result) =>
+        result.name.toLowerCase().startsWith(search.toLowerCase())
+      );
 
-    setSearchResults(filteredResults);
+      setSearchResults(filteredResults);
+    }
   };
 
   return (
@@ -45,7 +59,7 @@ function SearchBar() {
               <li key={index} className="results">
                 <span className="result-name">{result.name}</span>
                 <span className="result-symbol">{result.symbol}</span>
-                <span className="result-value">{result.value}</span>
+                <span className="result-value">{trade.value}</span>
                 <span className="result-percentage" style={{ color: "red" }}>
                   {result.percentage}
                 </span>
