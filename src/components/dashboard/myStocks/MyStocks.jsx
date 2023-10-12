6834c68bb6daa4953247f1711a66ed0c641a897f
diogocesar <132657506+diogoascesar@users.noreply.github.com>
2023-10-12 23:26:49 +0100

    files-fix-mystocks-ok

--- src/components/dashboard/myStocks/MyStocks.jsx
+++ src/components/dashboard/myStocks/MyStocks.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './myStocks.css';
 
-export default function MyStocks({props}){
+
+export default function MyStocks({symbol, name, total, percentage}){
+
     return(
         <>
             <div className="stock-item-container">
                 <div className='stock-labels-container'>
-                    {console.log("+paslrkoadk" + props.name)}
-                    <p className='symbol-label'>{props.symbol}</p>
-                    <p className='stock-first-name'>{props.name}</p>
+                    <p className='symbol-label'>{symbol}</p>
+                    <p className='stock-first-name'>{name}</p>
                 </div>
                 <div className="stock-values-container">
-                    <p className='stock-value'></p>
-                    <p className="stock-percentage"></p>
+                    <p className='stock-value'>
+                        {total}
+                    </p>
+                    <p className="stock-percentage">{percentage}%</p>
                 </div>
             </div>
-            <i className="divide-stock fi fi-rr-tally-1"></i>
+        <i className="divide-stock fi fi-rr-tally-1"></i>
         </>    
     );
 }
