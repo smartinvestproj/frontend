9886b414d3a37c5d33a76b8463861c438b361e70
Pedro <pedrodinisconceicao@gmail.com>
2023-10-20 17:22:15 +0100

    starting dashboard graph

--- src/components/dashboard/dashboardBody/DashboardGraph.jsx
+++ src/components/dashboard/dashboardBody/DashboardGraph.jsx
@@ -1,16 +1,38 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./dashboardGraph.css";
 import Chart from "chart.js/auto";
+import getTrades from "../../../services/getTrades";
 
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 const data = {
-  labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+  labels: [],
   datasets: [
     {
       label: "",
-      data: [12, 19, 10, 5, 7, 4, 7, 10, 12, 15, 6, 3],
+      data: [],
       backgroundColor: ["rgb(217,83,79)"],
       borderColor: ["rgb(217,83,79)"],
       borderWidth: 2,
@@ -20,63 +42,66 @@ const data = {
 
 function PortfolioPage() {
   const chartRef = useRef(null);
-  const [period, setPeriod] = useState("1 day"); // Estado para controlar o período
-
+  const [period, setPeriod] = useState("All"); // Estado para controlar o período
+  const [trades, setTrades] = useState([]);
 
   useEffect(() => {
-    
-
-    // Opções do gráfico (exemplo)
-    const options = {
-      responsive: true,
-      scales: {
-        y: {
-          beginAtZero: true,
-          grid: {
-            color: "rgba(198, 166, 83, 0.2)", // Define a cor do quadriculado horizontal
-          },
-          ticks: {
-            color: "white", // Define a cor das labels dos meses
-          },
-        },
-        x: {
-          grid: {
-            color: "rgba(198, 166, 83, 0.2)", // Define a cor do quadriculado horizontal
-          },
-          ticks: {
-            color: "white", // Define a cor das labels dos meses
-          },
-        },
-      },
-      plugins: {
-        legend: {
-          display: false, // Define a legenda como invisível
-        },
-      },
-      elements: {
-        point: {
-          radius: 0,
-          pointStyle: 'rect',
-        }
+    async function fetchTradeData() {
+      try {
+        const data = await getTrades();
+        const trades = data.data;
+        setTrades(trades);
+      } catch (error) {
+        console.log(error);
       }
-    };
-
-    const ctx = chartRef.current; // Obtenha a referência atual do elemento canvas
-
-    // Verifique se o gráfico já existe e destrua-o
-    if (ctx && ctx.chart) {
-      ctx.chart.destroy();
     }
+    fetchTradeData();
+  }, []);
 
-    // Crie uma nova instância do gráfico de linha (corrigido)
-    const newChart = new Chart(ctx, {
-      type: "line",
-      data: data,
-      options: options,
-    });
+  function maisValias(trade) {
+    let gains = 0;
+    trades.sell_price === null && (trades.sell_price = parseFloat(trades.total));
+    gains += parseFloat(trades.sell_price) - parseFloat(trades.total);
+    return gains;
+  }
 
-    // Armazene a referência ao gráfico no elemento canvas
-    ctx.chart = newChart;
+  function totalTradeValues(trade) {
+    if (trade.state === 1) {
+      return parseFloat(trade.total);
+    } else {
+      return 0;
+    }
+  }
+
+  function stocksSold(trade) {
+    if (trade.state === 0) {
+      return parseFloat(stock.total);
+    } else {
+      return 0;
+    }
+  }
+
+  function calculateAll(trades) {
+    const totalTrades = totalTradeValues(trades);
+    const stocksSoldValue = stocksSold(trades);
+    const totalGains = maisValias(trades);
+    const dividends = trades.map((trade) => trade.dividends);
+    
+    const total = totalTrades - stocksSoldValue + totalGains + dividends;
+    
+    return {
+      tradeId: trades.id,
+      total,
+    };
+  }
+
+  console.log('total trade values:' + totalTradeValues(trades));
+  console.log('stocks sold:' + stocksSold(trades));
+  console.log('mais valias:' + maisValias(trades));
+  console.log('total:' + calculateAll(trades));
+
+  useEffect(() => {
+    updatePeriod("All");
   }, [period]);
 
   // Função para atualizar o período do gráfico
@@ -87,9 +112,17 @@ function PortfolioPage() {
     let newData = null;
 
     if (newPeriod === "1 day") {
-      const hourIndex = (new Date().getHours());
-      const DayLabels = Array.from({ length: 23 }, (_, i) => [hourIndex - 23 + i]);
-      const hoursData = Array.from({ length: 24 }, () => Math.floor(Math.random() * 20)); // Generate random data
+      const currentDate = new Date();
+      const hourIndex = currentDate.getHours();
+
+      const DayLabels = [];
+      for (let i = 0; i < 24; i++) {
+        const hour = (hourIndex + i) % 24;
+        DayLabels.push(`${hour}:00`);
+      }
+
+      const hoursData = DayLabels.map(() => Math.floor(Math.random() * 20));
+
       newData = {
         labels: DayLabels,
         datasets: [
@@ -103,11 +136,21 @@ function PortfolioPage() {
         ],
       };
     } else if (newPeriod === "1 week") {
-      const dayIndex = (new Date().getDay());
-      const WeekLabels = Array.from({ length: 7 }, (_, i) => dayNames[dayIndex - 6 + i]);
-      const weekData = Array.from({ length: 7 }, () => Math.floor(Math.random() * 21)); // Generate random data up to 20
+      const currentDate = new Date();
+      const weekLabels = [];
+
+      for (let i = 7; i >= 1; i--) {
+        const date = new Date(currentDate);
+        date.setDate(currentDate.getDate() - i);
+        const dayOfWeek = date.getDay();
+        const dayName = dayNames[dayOfWeek];
+        weekLabels.push(dayName);
+      }
+
+      const weekData = weekLabels.map(() => Math.floor(Math.random() * 21));
+
       newData = {
-        labels: WeekLabels,
+        labels: weekLabels,
         datasets: [
           {
             label: "",
@@ -120,12 +163,19 @@ function PortfolioPage() {
       };
     } else if (newPeriod === "1 month") {
       const currentDate = new Date();
-      const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-      const MonthLabels = Array.from({ length: daysInMonth }, (_, i) => i + 1);
-      const monthData = Array.from({ length: daysInMonth }, () => Math.floor(Math.random() * 20)); // Gere dados aleatórios
+      const monthLabels = [];
+      const monthData = [];
+
+      for (let i = 1; i < 31; i++) {
+        const date = new Date(currentDate);
+        date.setDate(currentDate.getDate() - i);
+        const day = date.getDate();
+        monthLabels.unshift(day);
+        monthData.unshift(Math.floor(Math.random() * 20));
+      }
 
       newData = {
-        labels: MonthLabels,
+        labels: monthLabels,
         datasets: [
           {
             label: "",
@@ -137,9 +187,14 @@ function PortfolioPage() {
         ],
       };
     } else if (newPeriod === "3 months") {
-      const monthIndex = (new Date().getMonth());
-      const Months3Labels = Array.from({ length: 3 }, (_, i) => monthNames[monthIndex - 2 + i]);
-      const months3Data = Array.from({ length: 90 }, () => Math.floor(Math.random() * 20));
+      const monthIndex = new Date().getMonth();
+      const Months3Labels = Array.from(
+        { length: 3 },
+        (_, i) => monthNames[monthIndex - 2 + i]
+      );
+      const months3Data = Array.from({ length: 90 }, () =>
+        Math.floor(Math.random() * 20)
+      );
 
       newData = {
         labels: Months3Labels,
@@ -154,9 +209,14 @@ function PortfolioPage() {
         ],
       };
     } else if (newPeriod === "6 months") {
-      const monthIndex = (new Date().getMonth());
-      const Months6Labels = Array.from({ length: 6 }, (_, i) => monthNames[monthIndex - 5 + i]);
-      const months6Data = Array.from({ length: 180 }, () => Math.floor(Math.random() * 20)); // Generate random data
+      const monthIndex = new Date().getMonth();
+      const Months6Labels = Array.from(
+        { length: 6 },
+        (_, i) => monthNames[monthIndex - 5 + i]
+      );
+      const months6Data = Array.from({ length: 180 }, () =>
+        Math.floor(Math.random() * 20)
+      );
       newData = {
         labels: Months6Labels,
         datasets: [
@@ -169,12 +229,17 @@ function PortfolioPage() {
           },
         ],
       };
-    } 
-    else if (newPeriod === "1 year") {
-      const monthIndex = (new Date().getMonth());
-      const months12Labels = Array.from({ length: 12 }, (_, i) => monthNames[monthIndex - 11 + i]);
+    } else if (newPeriod === "1 year") {
+      const monthIndex = new Date().getMonth();
+      const months12Labels = [];
 
-      const yearData = Array.from({ length: 12 }, () => Math.floor(Math.random() * 20)); // Generate random data
+      for (let i = 0; i < 12; i++) {
+        const month = (monthIndex + i) % 12;
+        months12Labels.push(monthNames[month]);
+      }
+
+      const yearData = months12Labels.map(() => Math.floor(Math.random() * 20));
+
       newData = {
         labels: months12Labels,
         datasets: [
@@ -188,20 +253,42 @@ function PortfolioPage() {
         ],
       };
     } else if (newPeriod === "All") {
+      let minDate = new Date();
+      const currentDate = new Date();
+      let allLabels = [];
+      let AllData = calculateAll(trades);
+
+      trades.map((trade) => {
+        const date = new Date(trade.date);
+        if (date < minDate) {
+          minDate = date;
+        }
+      });
+
+      const timeDiff = currentDate - minDate;
+      const interval = timeDiff / 4;
+
+      allLabels.push(minDate.toLocaleDateString("pt-PT"));
+      for (let i = 1; i <= 3; i++) {
+        const intermediateDate = new Date(minDate.getTime() + i * interval);
+        allLabels.push(intermediateDate.toLocaleDateString("pt-PT"));
+      }
+      allLabels.push(currentDate.toLocaleDateString("pt-PT"));
+
       newData = {
-        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
-      datasets: [
-        {
-          label: "",
-          data: [12, 19, 10, 5, 7, 4, 7, 10, 12, 15, 6, 3],
-          backgroundColor: ["rgb(217,83,79)"],
-          borderColor: ["rgb(217,83,79)"],
-          borderWidth: 2,
-        },
-      ],
+        labels: allLabels,
+        datasets: [
+          {
+            label: "",
+            data: [0, AllData],
+            backgroundColor: ["rgb(217,83,79)"],
+            borderColor: ["rgb(217,83,79)"],
+            borderWidth: 2,
+          },
+        ],
       };
     }
-
+    
     const options = {
       responsive: true,
       scales: {
@@ -231,9 +318,9 @@ function PortfolioPage() {
       elements: {
         point: {
           radius: 0,
-          pointStyle: 'rect',
-        }
-      }
+          pointStyle: "rect",
+        },
+      },
     };
     const ctx = chartRef.current;
 
@@ -248,8 +335,6 @@ function PortfolioPage() {
     });
 
     ctx.chart = newChart;
-
-    
   };
 
   return (
@@ -272,10 +357,19 @@ function PortfolioPage() {
           <button onClick={() => updatePeriod("6 months")}>6 months</button>
           <button onClick={() => updatePeriod("1 year")}>1 year</button>
           <button onClick={() => updatePeriod("All")} className="all">
-            <i className="fi fi-rr-chart-line-up" style={{ marginRight: '5px' }}></i> All
+            <i
+              className="fi fi-rr-chart-line-up"
+              style={{ marginRight: "5px" }}
+            ></i>{" "}
+            All
           </button>
         </div>
-        <canvas ref={chartRef} id="overviewChart" width="400" height="150"></canvas>
+        <canvas
+          ref={chartRef}
+          id="overviewChart"
+          width="400"
+          height="150"
+        ></canvas>
       </div>
     </main>
   );
--- src/components/reports/pieChart/PieCharts.jsx
+++ src/components/reports/pieChart/PieCharts.jsx
@@ -27,12 +27,32 @@ function PieCharts(){
   const stockNames = tradeData.map((trade) => trade.stock.name);
   const quantities = tradeData.map((trade) => parseFloat(trade.quantity));  
 
+  
+
   const data = {
     labels: stockNames,
     datasets: [
       {
         data: quantities,
-        backgroundColor: ['#0668E1', '#A3AAAE', '#E4C083', '#D81F26'],
+        backgroundColor : [
+          '#0668E1',
+          '#A3AAAE',
+          '#E4C083',
+          '#D81F26',
+          '#FF5733',
+          '#12A4C6',
+          '#8CCE4A',
+          '#F5A623',
+          '#8E44AD',
+          '#D35400',
+          '#3498DB',
+          '#16A085',
+          '#E74C3C',
+          '#2ECC71',
+          '#9B59B6',
+          '#F39C12',
+          '#34495E'
+        ],
         borderColor: '#242424',
       },
     ],
